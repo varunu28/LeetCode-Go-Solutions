830f673d566de67334d9c4931ca82c8a1552da36
varunu28 <varun.u28@gmail.com>
2020-09-24 12:27:14 -0500

    Added number-of-steps-to-reduce-a-number-to-zero.go

---
+++ src/problem1342/number-of-steps-to-reduce-a-number-to-zero.go
@@ -0,0 +1,14 @@
+package problem1342
+
+func numberOfSteps(num int) int {
+	steps := 0
+	for num > 0 {
+		if num%2 == 0 {
+			num = num / 2
+		} else {
+			num--
+		}
+		steps++
+	}
+	return steps
+}
