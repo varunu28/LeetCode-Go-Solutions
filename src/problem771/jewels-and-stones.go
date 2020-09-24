a80a4a0d00846b62bd526f10a3c089bfc744dc2b
varunu28 <varun.u28@gmail.com>
2020-09-24 12:24:31 -0500

    Added jewels-and-stones.go

---
+++ src/problem771/jewels-and-stones.go
@@ -0,0 +1,15 @@
+package problem771
+
+import (
+	"strings"
+)
+
+func numJewelsInStones(J string, S string) int {
+	count := 0
+	for _, c := range S {
+		if strings.ContainsRune(J, c) {
+			count++
+		}
+	}
+	return count
+}
